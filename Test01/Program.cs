acb9c0428ac342f1b448e0860641f07c79f4645b
Ekaterina <alex40umov@gmail.com>
2022-11-14 17:51:29 +0300

    HomeWork09

---
+++ Test01/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int N = new Random().Next(1, 100);
+Console.WriteLine($"Ввод числа N = {N}");
+string result = NumberRecursive (N);
+Console.WriteLine($"Все числа от N до 1: {result}");
+
+string NumberRecursive(int N)
+{
+    if (N == 1) 
+    {
+        return "1";
+    }
+    else 
+    {
+        return N + "," + NumberRecursive(N-1);
+    }
+}
---
+++ Test03/Program.cs
@@ -0,0 +1,25 @@
+﻿// 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int m = new Random().Next(2, 4);
+int n = new Random().Next(2, 4);
+Console.WriteLine($"Введены числа m = {m} и n = {n}");
+int result = Akkerman(m, n);
+Console.Write($"Функция Аккермана = {result} ");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
